08456d580f3843c7ae63375422ba46303946fdf8
anvimagu <andersononca@hotmail.com>
2021-08-31 18:45:39 -0300

    projeto finalizado

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function redirecionar(){
+    window.open('https://www.youtube.com/watch?v=tKtV1InzvWM')
+}
+
+function redirecionar2(){
+    window.open('https://pt.wikipedia.org/wiki/Flash_(DC_Comics)')
+    
+}
